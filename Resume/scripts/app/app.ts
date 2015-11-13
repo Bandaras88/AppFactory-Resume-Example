e65e115d4d0522f9098cb6faa1209621462e020e
Kevesha Pillay <kevesha.pillay@outlook.com>
2015-11-13 15:59:46 +0200

    creating the CV

--- Resume/scripts/app/app.ts
+++ Resume/scripts/app/app.ts
@@ -7,7 +7,87 @@ interface indexScope extends angular.IScope {
     "use strict";
     var app = angular.module("app", []);
 
-    app.controller("indexController", ["$scope", function ($scope: indexScope) {
+    app.controller("indexController", ["$scope", function ($scope) {
         $scope.resume = new resume();
+        $scope.contactDetails = false;
+        $scope.skills = false;
+        $scope.studies = false;
+        $scope.workHistory = false;
+        $scope.references = false;
+        $scope.fullView = false;
+        $scope.btnContact = true;
+        $scope.btnStudies = true;
+        $scope.btnSkills = true;
+        $scope.btnWork = true;
+        $scope.btnRef = true;
+        $scope.btnFull = true;
+
+        $scope.showContactDetails = function () {
+            $scope.contactDetails = true;
+            $scope.skills = false;
+            $scope.studies = false;
+            $scope.workHistory = false;
+            $scope.references = false;
+            $scope.fullView = false;
+        };
+        $scope.showSkills = function () {
+            $scope.skills = true;
+            $scope.contactDetails = false;
+            $scope.studies = false;
+            $scope.workHistory = false;
+            $scope.references = false;
+            $scope.fullView = false;
+        };
+        $scope.showStudies = function () {
+            $scope.studies = true;
+            $scope.contactDetails = false;
+            $scope.skills = false;
+            $scope.workHistory = false;
+            $scope.references = false;
+            $scope.fullView = false;
+        };
+        $scope.showWorkHistory = function () {
+            $scope.workHistory = true;
+            $scope.contactDetails = false;
+            $scope.skills = false;
+            $scope.studies = false;
+            $scope.references = false;
+            $scope.fullView = false;
+        };
+        $scope.showReferences = function () {
+            $scope.references = true;
+            $scope.contactDetails = false;
+            $scope.skills = false;
+            $scope.studies = false;
+            $scope.workHistory = false;
+            $scope.fullView = false;
+        };
+        $scope.showFullCV = function () {
+            $scope.fullView = true;
+            $scope.contactDetails = false;
+            $scope.skills = false;
+            $scope.studies = false;
+            $scope.references = false;
+            $scope.btnContact = false;
+            $scope.btnStudies = false;
+            $scope.btnSkills = false;
+            $scope.btnWork = false;
+            $scope.btnRef = false;
+            $scope.btnFull = false;
+            $scope.references = false;
+        };
+        $scope.back = function () {
+            $scope.fullView = false;
+            $scope.contactDetails = false;
+            $scope.skills = false;
+            $scope.studies = false;
+            $scope.references = false;
+            $scope.btnContact = true;
+            $scope.btnStudies = true;
+            $scope.btnSkills = true;
+            $scope.btnWork = true;
+            $scope.btnRef = true;
+            $scope.btnFull = true;
+        };
     }]);
 })();
--- Resume/scripts/resume.ts
+++ Resume/scripts/resume.ts
@@ -1,17 +1,63 @@
 ﻿class resume {
     name: string = "Kevesha Pillay";
+    currentTitle: string = "Window cleaner";
     contacts = [
         {
-            label: "email",
-            value: "kpillay@awesome.ninja"
+            label: "Email Address",
+            value: "kpillay@awesome.ninja",
         },
         {
-            label: "phone",
+            label: "Phone Number",
             value: "+27 555 0123"
+        },
+        {
+            label: "Twitter",
+            value: "@awesomeSuperMe"
         }
-    ]; 
-    studies = [];
-    workHistory = [];
-    skills = [];
-    refernces = [];
+    ];
+    studies = [
+        {
+            value: "The University of awesome stars",
+            subjects: "Maths, English, Afrikaans",
+            dates: "21st January 2013 - 23rd December 2015"
+        },
+        {
+            value: "The University of awesome stars",
+            subjects: "Maths, English, Afrikaans",
+            dates: "21st January 2013 - 23rd December 2015"
+        },
+        {
+            value: "The University of awesome stars",
+            subjects: "Maths, English, Afrikaans",
+            dates: "21st January 2013 - 23rd December 2015"
+        }
+    ];
+    workHistory = [
+        {
+            companyName: "Clowns dress Up",
+            duties: "Smiling and Having Fun",
+            duration: "17th January 2015 to 30th December 2015"
+        },
+        {
+            companyName: "Lorem Ipsum",
+            duties: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim.Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend.Ut nonummy.",
+            duration: "17th January 2015 to 30th December 2015"
+        },
+    ];
+    skills = ["Friendly and Kind", "Happy personality", "Problem Solving"];
+    references = [
+        {
+            Name: "Mrs Pillay",
+            phonenumber: "+27 123 1234",
+            company: "The wigwam company",
+            role: "The Chief of pastry",
+            email: "this@this.this"
+        },
+        {
+            Name: "Mrs Pillay",
+            phonenumber: "+27 123 1234",
+            company: "The wigwam company",
+            role: "The Chief of pastry",
+            email: "this@this.this"
+        }];
 }
